da552efedfce4d5ada2b089da2906b9bab6763d9
r <radu@raicea.com>
2018-02-04 20:03:28 -0500

    add recipes controller and views

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,50 @@
+class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    redirect_to recipes_path
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name)
+    end
+end
